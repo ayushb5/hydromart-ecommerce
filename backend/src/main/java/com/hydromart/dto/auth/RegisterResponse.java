59d1db185dbd85c80e7270e0ec844234aa2cc93e
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-08-17 17:41:38 +0530

    refactor(auth): improve user registration response

---
+++ backend/src/main/java/com/hydromart/dto/auth/RegisterResponse.java
@@ -0,0 +1,15 @@
+package com.hydromart.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterResponse {
+	private String message;
+	private String email;
+}
