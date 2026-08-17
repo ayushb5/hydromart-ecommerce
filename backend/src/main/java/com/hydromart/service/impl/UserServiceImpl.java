cb0d0c3532012cc5a6efcf4b5f5d795ba59aaf85
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-08-17 21:28:46 +0530

    refractor: change in annotation for constructor

--- backend/src/main/java/com/hydromart/service/impl/UserServiceImpl.java
+++ backend/src/main/java/com/hydromart/service/impl/UserServiceImpl.java
@@ -20,16 +20,14 @@ import com.hydromart.exception.UserNotFoundException;
 import com.hydromart.repository.UserRepository;
 import com.hydromart.service.UserService;
 
+import lombok.RequiredArgsConstructor;
+
 @Service
+@RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
 
 	private final UserRepository userRepository;
 	private final PasswordEncoder passwordEncoder;
-
-	public UserServiceImpl(UserRepository userRepository, PasswordEncoder passwordEncoder) {
-		this.userRepository = userRepository;
-		this.passwordEncoder = passwordEncoder;
-	}
 
 	@Override
 	public RegisterResponse registerUser(RegisterRequest request) {
