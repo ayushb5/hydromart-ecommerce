7a5edf8164a03a50a2500ec5be85f5171c9640ad
Ayush <179284688+ayushb5@users.noreply.github.com>
2026-08-18 11:12:25 +0530

    refractor(config): temporary allowing to access all api's without jwt

--- backend/src/main/java/com/hydromart/config/SecurityConfig.java
+++ backend/src/main/java/com/hydromart/config/SecurityConfig.java
@@ -1,8 +1,8 @@
 package com.hydromart.config;
 
 import org.springframework.context.annotation.Bean;
+
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -17,16 +17,14 @@ public class SecurityConfig {
 	}
 	
 	@Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+	public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
-        http
-            .csrf(csrf -> csrf.disable())
-            .authorizeHttpRequests(auth -> auth
-                .requestMatchers("/api/auth/**").permitAll()
-                .anyRequest().authenticated()
-            )
-            .httpBasic(Customizer.withDefaults());
+	    http
+	        .csrf(csrf -> csrf.disable())
+	        .authorizeHttpRequests(auth -> auth
+	            .anyRequest().permitAll()
+	        );
 
-        return http.build();
-    }
+	    return http.build();
+	}
 }
